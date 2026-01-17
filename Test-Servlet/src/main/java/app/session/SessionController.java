4ab13143ab6c1014135ff8fd7ef5ae347e67afa1
Andriatsara Iratra Fernand <Iratra53@gmail.com>
2026-01-17 18:47:29 +0300

    Sprint 11 bis : Test des gestions des rôles

--- Test-Servlet/src/main/java/app/session/SessionController.java
+++ Test-Servlet/src/main/java/app/session/SessionController.java
@@ -2,6 +2,7 @@ package app.test;
 
 import framework.annotation.*;
 import framework.util.FrameworkSession;
+import framework.annotation.AuthAnnot;
 
 @ControllerAnnot
 public class SessionController {
@@ -12,20 +13,49 @@ public class SessionController {
     public String login(FrameworkSession session) {
         session.put("user_role", "ADMIN");
         session.put("user_email", "test@gmail.com");
+        session.put("auth_user", "employe");
         return "Vous êtes connecté ! (Session ID stocké)";
     }
 
     // Dashboard : On lit la session
-    @UrlAnnot("/dashboard")
+    @UrlAnnot("/frontoffice")
+    @AuthAnnot("employe")
     @MethodMapping("GET")
-    public String dashboard(FrameworkSession session) {
+    public String frontoffice(FrameworkSession session) {
         Object email = session.get("user_email");
         
         if (email == null) {
             return "Vous n'êtes pas connecté.";
         }
         
-        return "Bienvenue sur le dashboard, " + email.toString();
+        return "Bienvenue sur le dashboard, " + email.toString() + " seules les employes peuvent voir ceci.";
+    }
+
+     // Dashboard : On lit la session
+    @UrlAnnot("/backoffice")
+    @AuthAnnot("manager")
+    @MethodMapping("GET")
+    public String backoffice(FrameworkSession session) {
+        Object email = session.get("user_email");
+        
+        if (email == null) {
+            return "Vous n'êtes pas connecté.";
+        }
+        
+        return "Bienvenue sur le dashboard, " + email.toString() + " seules les managers peuvent voir ceci.";
+    }
+
+    @UrlAnnot("/all")
+    @AuthAnnot("")
+    @MethodMapping("GET")
+    public String all(FrameworkSession session) {
+        Object email = session.get("user_email");
+        
+        if (email == null) {
+            return "Vous n'êtes pas connecté.";
+        }
+        
+        return "Bienvenue sur le dashboard, " + email.toString() + " toutes les personnes connectées peuvent voir ceci.";
     }
     
     // Logout : On détruit la session
